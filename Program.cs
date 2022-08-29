0b7aef9c67a325abae3a6b3a19be9bb30f2836f0
Leonardo T. Lombardi <lombardi.leo@gmail.com>
2022-08-29 17:19:42 -0300

    Fixing console output and one example code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This is one example of a exercise from Hackerhank, working in vscode
+class Result
+{
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<int> h, string word)
+    {
+        var max = 0;
+        foreach (var letter in word)
+        {
+            var index = letter - 'a';
+            if (max < h[index])
+            {
+                max = h[index];
+            }
+        }
+        return max;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(Console.OpenStandardOutput());
+
+        List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+        string word = Console.ReadLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
